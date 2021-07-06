e001329c6f7609fb26fedff8f7ebe0d913abc504
Arvind_Suresh <53540697+Arvind0515@users.noreply.github.com>
2021-07-07 00:46:06 +0530

    SETTING UP OF REST API

--- bin/main.dart
+++ bin/main.dart
@@ -1,17 +1,64 @@
 import 'package:dart_mongo/dart_mongo.dart' as dart_mongo;
 import 'package:mongo_dart/mongo_dart.dart';
 import 'package:mongo_dart/mongo_dart.dart';
+import 'dart:io';
 
 main(List<String> arguments) async {
+  int port = 8085;
+  var server = await HttpServer.bind('localhost', port);
   Db db = Db('mongodb://localhost:27017/test');
   await db.open();
   print("connected");
+  // await db.close();
+  // await server.close();
 
   DbCollection coll = db.collection('people');
+  var people = await coll.find().toList();
+  print(people);
+  // await db.close();
+  // await server.close();
+  server.listen((HttpRequest request) async {
+    // if (request.uri.path == '/') {
+    //   request.response.write('hello world');
+    //   //await request.response.close();
+    //   // await db.close();
+    //   // await server.close();
+    // } else if (request.uri.path == '/people') {
+    //   request.response.write(await coll.find().toList());
+    //   //await request.response.close();
+    //   // await db.close();
+    //   // await server.close();
+    // }
+    switch (request.uri.path) {
+      case '/':
+        request.response.write('hello world');
+        await request.response.close();
+        break;
+      case '/people':
+        request.response.write(await coll.find().toList());
+        await request.response.close();
+        break;
+      default:
+        request.response
+          ..statusCode = HttpStatus.notFound
+          ..write('Not Found');
+        await request.response.close();
+    }
+    print("xxxxxxxxxxxxxxxxxxxxxx");
+    await request.response.close();
+    // await db.close();
+    // await server.close();
+
+    // await db.close();// await server.close();
+  });
+  print("yyyyyyyyyyxxxxxxxxxxxxxx");
+  // await db.close();
+  // await server.close();
+//  await db.close();
 
   ///lets read the db:
-  var people =
-      await coll.find().toList(); //returns all the data in the collection
+  // var people =
+  //     await coll.find().toList(); //returns all the data in the collection
 
   //await coll.find(where.eq("first_name", "Hilton")).toList(); //returns datas whose first name equals to "Hilton";
 
@@ -24,24 +71,24 @@ main(List<String> arguments) async {
   // print(people);
 
   ///lets create a new document in database called test collection called people.
-  await coll.save({
-    'id': 101,
-    'first_name': 'Arvind',
-    'email': 'arvindsuresh2002@gmail.com',
-    'gender': 'male',
-    'ip_address': '63.90.69.20',
-  }); //this is how we save a new document
+  // await coll.save({
+  //   'id': 101,
+  //   'first_name': 'Arvind',
+  //   'email': 'arvindsuresh2002@gmail.com',
+  //   'gender': 'male',
+  //   'ip_address': '63.90.69.20',
+  // }); //this is how we save a new document
 
   // var people = await coll.findOne(where.eq('first_name', 'Arvind'));
   // print(people); // printing the document saved.
 
   ///lets update the document saved.
 
-  await coll.update(await coll.findOne(where.eq('first_name', 'Arvind')), {
-    r'$set': {
-      'first_name': 'Arvind S',
-    }
-  }); // thats how we update a specific field in an document keeping other fields ideal
+  // await coll.update(await coll.findOne(where.eq('first_name', 'Arvind')), {
+  //   r'$set': {
+  //     'first_name': 'Arvind S',
+  //   }
+  // }); // thats how we update a specific field in an document keeping other fields ideal
 
   // await coll.update(await coll.findOne(where.eq('first_name', 'Arvind')), {
   //     'first_name': 'Arvind S',
@@ -51,11 +98,9 @@ main(List<String> arguments) async {
 
   ///lets remove a document
 
-  await coll.remove(await coll.findOne(where.eq('id', 101)));
-  await coll
-      .remove(where.eq('id', 101)); //removes all the document whose id is 101
-  // people = await coll.find(where.eq('id', 101)).toList();
-  // print(people); //it should print null;
-
-  await db.close(); //it closes the db
+  // await coll.remove(await coll.findOne(where.eq('id', 101)));
+  // await coll
+  //     .remove(where.eq('id', 101)); //removes all the document whose id is 101
+  //people = await coll.find(where.eq('id', 101)).toList();
+  //print(people); //it should print null;
 }
